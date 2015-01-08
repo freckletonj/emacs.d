e4bc020fec66f79045af272cc24bc43b390c0905
Josh Freckleton <freckletonj@gmail.com>
2015-01-07 22:46:33 -0700

    ipython support through ein package

---
+++ elpa/websocket-20141226.1957/websocket-autoloads.el
@@ -0,0 +1,18 @@
+;;; websocket-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("websocket-pkg.el" "websocket.el") (21678
+;;;;;;  4371 354429 140000))
+
+;;;***
+
+(provide 'websocket-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; websocket-autoloads.el ends here
